14997aa8dc9f9de98d35ac599a62462d33d27a98
ulisestc <ulisestc27@gmail.com>
2026-05-28 03:06:15 -0600

    small fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   apiUrls: {
     auth:        'http://localhost:9000/api/auth',
     periodos:    'http://localhost:9000/api/periodos',
+    materias:    'http://localhost:9000/api/periodos',
     docentes:    'http://localhost:9000/api/docentes',
     asistencias: 'http://localhost:9000/api/asistencias',
     reportes:    'http://localhost:9000/api/reportes',
